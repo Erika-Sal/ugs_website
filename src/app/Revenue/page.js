163500f2f5d2543fce4e635c8af259c44dbfc3ac
Erika <erikasal832@gmail.com>
2024-12-02 18:19:19 -0600

    updating images

--- src/app/Revenue/page.js
+++ src/app/Revenue/page.js
@@ -16,13 +16,13 @@ export default function page() {
  
       <div className="Images">
       <img
-          src="https://wallpapers.com/images/hd/microsoft-logo-modern-design-yog3vbpgtu79aasj.png"
+          src="/microsoft.png"
           style={{ width: '100px', height: 'auto' }} alt=""/>
       <img
-          src="https://logodownload.org/wp-content/uploads/2013/12/apple-logo-5.png"
+          src="/apple.png"
           style={{ width: '100px', height: 'auto' } } alt=""/>
       <img
-          src="https://download.logo.wine/logo/Oracle_Corporation/Oracle_Corporation-Logo.wine.png"
+          src="/oracle.png"
           style={{ width: '150px', height: 'auto' }} alt=""/>
 
 
@@ -57,13 +57,13 @@ export default function page() {
 
       <div className="pieChart">
       <img
-        src="https://i.ibb.co/87ZZzrF/microsoft-Finish.png"
+        src="/microsoftDems.png"
         style={{ width: '250px', height: 'auto' }} alt=""/>
       <img
-        src="https://i.ibb.co/L6rCT13/apple-Finish.png"
+        src="/appleDems.png"
         style={{ width: '250px', height: 'auto' }} alt=""/>
       <img
-        src="https://i.ibb.co/82LH5dz/oracle-Finish.png"
+        src="/oracleDems.png"
         style={{ width: '250px', height: 'auto' }} alt=""/>  
       </div>
      </>
